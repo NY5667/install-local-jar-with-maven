812c4ab37ccb3380f3517bdc3ac4b27ac24ee4e3
NY5667 <82657677+NY5667@users.noreply.github.com>
2023-03-13 10:38:23 +0800

    init

---
+++ src/main/java/com/sqlparser/gsqlparser/GsqlparserApplication.java
@@ -0,0 +1,13 @@
+package com.sqlparser.gsqlparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsqlparserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsqlparserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sqlparser/gsqlparser/GsqlparserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqlparser.gsqlparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsqlparserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
